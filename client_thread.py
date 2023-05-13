6fd9879fc27cef7224dae089fdfe9f1a1568e9c7
Poodoop <55837575+Poodoop@users.noreply.github.com>
2023-05-13 23:01:48 +0700

    Update client_thread.py

--- client_thread.py
+++ client_thread.py
@@ -11,3 +11,29 @@ def kirim_data():
     server_address = ('localhost', 45000)
     logging.warning(f"opening socket {server_address}")
     sock.connect(server_address)
+
+    try:
+        # Send data
+        message = 'TIME\r\n'
+        logging.warning(f"[CLIENT] sending {message}")
+        sock.sendall(message.encode())
+        # Look for the response
+        data = sock.recv(32)
+        logging.warning(f"[FROM SERVER] got {data}")
+    finally:
+        logging.warning("closing")
+        sock.close()
+    return
+
+def thread():
+    t = threading.Thread(target=kirim_data)
+    t.start()
+    t.join()
+
+if __name__ == '__main__':
+    thread_counter = 0 
+    start_time = time.time()  
+    while time.time() - start_time < 30:  
+        thread()
+        thread_counter += 1  
+    logging.warning(f"Total threads created: {thread_counter}")
