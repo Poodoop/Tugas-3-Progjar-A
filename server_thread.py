3f3f15ddcf3fb029cf6edb7a31719b8b0b1078d2
Poodoop <55837575+Poodoop@users.noreply.github.com>
2023-05-13 21:17:24 +0700

    Create server_thread.py

---
+++ server_thread.py
@@ -0,0 +1,50 @@
+from socket import *
+import socket
+import threading
+import logging
+import time
+import sys
+
+class ProcessTheClient(threading.Thread):
+	def __init__(self,connection,address):
+		self.connection = connection
+		self.address = address
+		threading.Thread.__init__(self)
+
+	def run(self):
+		while True:
+			data = self.connection.recv(32)
+			#cek request
+			if data.startswith('TIME') and data.endswith('\r\n'):
+        			timenow = time.strftime("%H:%M:%S")
+        			response = f"JAM {timenow}\r\n"
+        			logging.warning(f"sending {response} to {self.address}")
+				self.connection.sendall(response.encode('utf-8'))
+			else:
+				break
+		self.connection.close()
+
+class Server(threading.Thread):
+	def __init__(self):
+		self.the_clients = []
+		self.my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		threading.Thread.__init__(self)
+
+	def run(self):
+		self.my_socket.bind(('0.0.0.0',45000))
+		self.my_socket.listen(1)
+		while True:
+			self.connection, self.client_address = self.my_socket.accept()
+			logging.warning(f"connection from {self.client_address}")
+			
+			clt = ProcessTheClient(self.connection, self.client_address)
+			clt.start()
+			self.the_clients.append(clt)
+	
+
+def main():
+	svr = Server()
+	svr.start()
+
+if __name__=="__main__":
+	main()
