bad0d2fa7c4beaf214d33d305ad404c0fc349f63
Poodoop <55837575+Poodoop@users.noreply.github.com>
2023-05-14 22:36:17 +0700

    Update server_thread.py

--- server_thread.py
+++ server_thread.py
@@ -5,46 +5,67 @@ import logging
 import time
 import sys
 
-class ProcessTheClient(threading.Thread):
-	def __init__(self,connection,address):
-		self.connection = connection
-		self.address = address
-		threading.Thread.__init__(self)
 
-	def run(self):
-		while True:
-			data = self.connection.recv(32)
-			#cek request
-			if data.startswith('TIME') and data.endswith('\r\n'):
-        			timenow = time.strftime("%H:%M:%S")
-        			response = f"JAM {timenow}\r\n"
-        			logging.warning(f"[Server] sending {response} to {self.address}")
-				self.connection.sendall(response.encode('utf-8'))
-			else:
-				break
-		self.connection.close()
+class ProcessTheClient(threading.Thread):
+    def __init__(self, connection, address, server):
+        self.connection = connection
+        self.address = address
+        self.server = server
+        threading.Thread.__init__(self)
+
+    def run(self):
+        while True:
+            data = self.connection.recv(32)
+            if data:
+                logging.warning(
+                    f"[Server] received {data} from {self.address}")
+
+                if data.startswith('TIME') and data.endswith('\r\n'):
+                    current_time = time.strftime("%H:%M:%S")
+                    response = f"JAM {current_time}\r\n"
+                    logging.warning(
+                        f"[Server] sending {response} to {self.address}")
+                    self.connection.sendall(response.encode('utf-8'))
+
+                    self.server.update_response_count()
+                else:
+                    break
+            else:
+                break
+        self.connection.close()
+
 
 class Server(threading.Thread):
-	def __init__(self):
-		self.the_clients = []
-		self.my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		threading.Thread.__init__(self)
+    def __init__(self):
+        self.the_clients = []
+        self.my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.client_count = 0
+        self.response_count = 0 
+        threading.Thread.__init__(self)
 
-	def run(self):
-		self.my_socket.bind(('0.0.0.0',45000))
-		self.my_socket.listen(1)
-		while True:
-			self.connection, self.client_address = self.my_socket.accept()
-			logging.warning(f"connection from {self.client_address}")
-			
-			clt = ProcessTheClient(self.connection, self.client_address)
-			clt.start()
-			self.the_clients.append(clt)
-	
+    def run(self):
+        self.my_socket.bind(('0.0.0.0', 45000))
+        self.my_socket.listen(1)
+        while True:
+            self.connection, self.client_address = self.my_socket.accept()
+            logging.warning(f"connection from {self.client_address}")
+
+            clt = ProcessTheClient(self.connection, self.client_address, self)
+            clt.start()
+            self.the_clients.append(clt)
+            self.client_count += 1
+            logging.warning(f"Total clients connected: {self.client_count}")
+
+    def update_response_count(self):
+        self.response_count += 1
+        logging.warning(f"Total responses sent: {self.response_count}")
+
 
 def main():
-	svr = Server()
-	svr.start()
+    svr = Server()
+    svr.start()
 
-if __name__=="__main__":
-	main()
+
+if __name__ == "__main__":
+    main()
